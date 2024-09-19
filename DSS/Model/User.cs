e189e7d49133947f324be2f36c03a40cf8ca4dd0
KEVIN STIVEN RIVERA OLARTE <ksrivera@ucundinamarca.edu.co>
2024-09-19 14:25:41 -0600

    Agregue la carpeta User

---
+++ DSS/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace DSS.Model
+{
+    public class User
+    {
+        int id {  get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public User() { }
+
+    }
+}
