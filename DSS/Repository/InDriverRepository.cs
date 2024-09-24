820bb287df1c89df63afd6ae7ce0c4d0eebb98bd
KEVIN STIVEN RIVERA OLARTE <ksrivera@ucundinamarca.edu.co>
2024-09-23 20:55:30 -0600

    All Drivers

---
+++ DSS/Repository/InDriverRepository.cs
@@ -0,0 +1,62 @@
+﻿using DSS.Context;
+using DSS.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSS.Repository
+{
+    public interface InDriverRepository
+    {
+        Task<Driver> GetDriversByIdAsync(int id);
+        Task<IEnumerable<Driver>> GetAllDriversAsync();
+        Task CreateDriversAsync(Driver driver);
+        Task UpdateDriversAsync(Driver driver);
+        Task DeleteDriverAsync(int id);
+
+
+    }
+    public class DriverRepository : InDriverRepository
+    {
+        private readonly SGITContex _context;
+
+        public DriverRepository(SGITContex context)
+        {
+            _context = context;
+        }
+        public async   Task<IEnumerable<Driver>> GetAllDriversAsync()
+        {
+            return await _context.Drivers
+                .Where(s => !s.IsDeleted) // Excluye eliminados
+                .ToListAsync();
+        }
+
+        public async Task<Driver> GetDriversByIdAsync(int id)
+        {
+            return await _context.Drivers
+                .FirstOrDefaultAsync(s => s.DriverId == id && !s.IsDeleted);
+        }
+
+        public async Task DeleteDriverAsync(int id)
+        {
+            var subject = await _context.Drivers.FindAsync(id);
+            if (subject != null)
+            {
+                subject.IsDeleted = true;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task CreateDriversAsync(Driver subject)
+        {
+            await _context.Drivers.AddAsync(subject);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateDriversAsync(Driver subject)
+        {
+            _context.Drivers.Update(subject);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
---
+++ DSS/Services/InDriverServices.cs
@@ -0,0 +1,53 @@
+﻿using DSS.Context;
+using DSS.Model;
+using DSS.Repository;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace DSS.Services
+{
+    public interface InDriverServices
+    {
+        Task<IEnumerable<Driver>> GetAllDriversAsync();
+        Task<Driver> GetDriversByIdAsync(int id);
+        Task CreateDriversAsync(Driver Driver);
+        Task UpdateDriversAsync(Driver Driver);
+        Task DeleteDriversAsync(int id);
+    }
+    public class DriverServices : InDriverServices
+    {
+        private readonly InDriverRepository _DriverRepository;
+
+        public DriverServices(InDriverRepository DriverRepository)
+        {
+            _DriverRepository = DriverRepository;
+        }
+
+        public async Task<IEnumerable<Driver>> GetAllDriversAsync()
+        {
+            return await _DriverRepository.GetAllDriversAsync();
+        }
+
+        public async Task<Driver> GetDriversByIdAsync(int id)
+        {
+            return await _DriverRepository.GetDriversByIdAsync(id);
+        }
+
+        public async Task CreateDriversAsync(Driver Driver)
+        {
+            await _DriverRepository.CreateDriversAsync(Driver);
+        }
+
+        public async Task UpdateDriversAsync(Driver Driver)
+        {
+            await _DriverRepository.UpdateDriversAsync(Driver);
+        }
+
+        public async Task DeleteDriversAsync(int id)
+        {
+            await _DriverRepository.DeleteDriverAsync(id);
+        }
+    }
+}
