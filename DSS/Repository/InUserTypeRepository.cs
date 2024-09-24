0110a6ba7f015b1479d28df6db7854f5fb766867
JUAN DIEGO PEREZ RIAÑO <diegoo.perez889@hotmail.com>
2024-09-23 21:45:26 -0500

    Usertype2

--- DSS/Repository/InUserTypeRepository.cs
+++ DSS/Repository/InUserTypeRepository.cs
@@ -1,4 +1,6 @@
-﻿using DSS.Model;
+﻿using DSS.Context;
+using DSS.Interfaces;
+using DSS.Model;
 
 namespace DSS.Repository
 {
@@ -9,5 +11,45 @@ namespace DSS.Repository
         Task AddAsync(UserType user);
         Task UpdateAsync(UserType user);
         Task DeleteAsync(int id);
+    }
+    public class UserTypeRepository : InUserTypeRespository
+    {
+        private readonly SGITContex _context;
+        public UserTypeRepository(SGITContex context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<UserType>> GetAllUserTypeAsync()
+        {
+            return await _context.userTypes
+                .Where(s => !s.IsDelete)
+                .ToListAsync();
+        }
+        public async Task<UserType> GetUserByUserTypeIdAsync(int id)
+        {
+            return await _context.userTypes
+                .FirstOrDefaultAsync(s => s.UserTypeId == id && !s.IsDelete);
+        }
+        public async Task CreateUserTypeAsync(UserType userType)
+        {
+            await _context.userTypes.CreateUserTypeAsync(userType);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateUserTypeAsync(UserType userType)
+        {
+            await _context.userTypes.UpdateAsync(userType);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task SoftDeleteUserTypeAsync(int id)
+        {
+            var UserType = await _context.userTypes.FindAsync(id);
+            if (UserType != null)
+            {
+                UserType.IsDelete = true;
+                await _context.SaveChangesAsync();
+            }
+        }
+
     }
 }
---
+++ DSS/Service/InUserTypeService.cs
@@ -0,0 +1,45 @@
+﻿using DSS.Interfaces;
+using DSS.Model;
+using DSS.Repository;
+
+namespace DSS.Service
+{
+    public interface InUserTypeService
+    {
+        Task<IEnumerable<UserType>> GetAllUserTypeAsync();
+        Task<UserType> GetUserByUserTypeIdAsync(int userTypeId);
+        Task<UserType> CreateUserTypeAsync(User userType);
+        Task<UserType> UpdateUserTypeAsync(User userTpy);
+        Task<UserType> SoftDeleteUserTypeAsync(int userTypeId);
+    }
+    public class UserTypeService : InUserTypeService
+    {
+        private readonly InUserTypeRespository _userTypeRepository;
+
+        public UserTypeService(InUserTypeRespository userTypeRepository)
+        {
+            _userTypeRepository = userTypeRepository;
+        }
+        public async Task<IEnumerable<UserType>> GetAllUserTypeAsync()
+        {
+            return await _userTypeRepository.GetAllUserTypeAsync();
+        }
+        public async Task<UserType> GetUserByUserTypeIdAsync(int userTypeId)
+        {
+            return await _userTypeRepository.GetUserByUserIdAsync(userTypeId);
+        }
+        public async Task CreateUserTypeAsync(UserType userType)
+        {
+            await _userTypeRepository.CreateUserAsync(userType);
+        }
+        public async Task UpdateUserTypeAsync(UserType userType)
+        {
+            await _userTypeRepository.UpdateUserAsync(userType);
+        }
+        public async Task SoftDeleteUserTypeAsync(int userTypeId)
+        {
+            await _userTypeRepository.SoftDeleteUserAsync(userTypeId);
+        }
+
+    }
+}
