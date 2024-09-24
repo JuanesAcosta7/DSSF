e9bf5fa2b601dd5b8eb04df15aab608ec7e28dd5
WILLIAM YESSID GARCIA TORRES <wygarcia@ucundinamarca.edu.co>
2024-09-23 22:58:37 -0500

    packages - Users

---
+++ DSS/Model/User.cs
@@ -0,0 +1,18 @@
+﻿namespace DSS.Model
+{
+    public class User
+    {
+        public required int Id { get; set; }
+
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+
+        public required string Username { get; set; }
+
+        public virtual required UserType UserType { get; set; }
+
+
+    }
+}
---
+++ DSS/Model/UserType.cs
@@ -0,0 +1,10 @@
+﻿namespace DSS.Model
+{
+    public class UserType
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+    }
+}
