1d811dca56c39b8a6125e827fa3ce89be3f423bf
Wygarcia <wiyegato@gmail.com>
2024-09-24 07:56:12 -0500

    Update InRolRepository.cs

--- DSS/Repository/InRolRepository.cs
+++ DSS/Repository/InRolRepository.cs
@@ -1,13 +1,2 @@
 
 
-namespace DSS.Repository
-{
-    public interface InRolRepository
-    {
-        Task<Rol> GetByRolIdAsync(int id);
-        Task<IEnumerable<Rol>> GetAllAsync();
-        Task Async(Rol rol);
-        Task UpdateAsync(Rol rol);
-        Task DeleteAsync(int id);
-    }
-}
