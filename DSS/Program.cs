ac94a9c376b6c4f36e7ef42d11fdb76c46e6eae6
JUAN ESTEBAN ACOSTA ALCALÁ <jestebanacosta@ucundinamarca.edu.co>
2024-10-06 08:33:47 -0500

    BACKEND Listo para ser publicado, con la cadena de conexion de la base de datos en somee

--- DSS/Program.cs
+++ DSS/Program.cs
@@ -37,11 +37,10 @@ builder.Services.AddScoped<InVehicleServices, VehiclesServices>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
